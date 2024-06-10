c689b2af93f88bedc349ebf7990b2428acb681f0
Pratyush shankar <pratyushshanker07@gmail.com>
2024-06-10 17:51:01 +0530

    adding the files

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React,{useState} from "react";
+import data from "./data";
+import Tours from "./components/Tours";
+
+const App = () => {
+ 
+  const [tours,setTours] =useState(data);
+
+  function removeTour(id) {
+    const newTours =tours.filter(tours =>tours.id !==id);
+    setTours(newTours);
+  }
+ 
+  if(tours.length===0) {
+    return (
+      <div className="refresh">
+        <h2>No Tours Left</h2>
+        <button className="btn-white" onClick={()=> setTours(data)}>
+        refresh
+        </button>
+      </div>
+    )
+  }
+ return <div className="App">
+  
+    <Tours tours={tours} removeTour={removeTour}></Tours>
+  </div>;
+};
+
+export default App;
